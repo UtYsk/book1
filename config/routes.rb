8e5414da688fd100948e24dcd4120eb31f30aee8
UtYsk <ueta.yusuke.infratop@gmail.com>
2019-11-21 22:32:25 +0900

    2nd

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'books/new'
-  get '' => 'books#top'
+  root :to => 'books#top'
   post 'books' => 'books#create'
 end
